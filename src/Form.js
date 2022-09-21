3735f304f4821698e4b2fcebff61ecc662da8428
YuliiaPaziura <yuliiapaziura@gmail.com>
2022-09-21 15:36:48 +0300

    use formik

--- src/Form.js
+++ src/Form.js
@@ -1,31 +1,96 @@
+import { FormikConsumer, useFormik } from "formik";
+
+// FORMIK
+// library for creating forms
+
+// create variable with useFormik hook
+// pass object with initial states and onSubmit function
+
+const validate = (values) => {
+    const errors = {};
+    if (!values.name) {
+        errors.name = 'Enter name';
+    }
+    else if (values.name.length <2) {
+        errors.name = 'Too short';
+    }
+
+    if (!values.email) {
+        errors.email = 'Enter email'
+    }
+    else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Enter valid email'
+    }
+
+    return errors
+}
 
 
 const Form = () => {
+    const formik = useFormik({
+        initialValues:{
+            name: '',
+            email:'',
+            amount: 10,
+            currency: '',
+            text: '',
+            terms: false
+        },
+        // validation function
+        // another way to write validate: validate,
+        validate,
+        onSubmit: values => console.log(JSON.stringify(values, null, 2))
+    })
+
     return (
-        <form className="form">
+        // add onSubmit attribute
+        <form className="form" onSubmit={formik.handleSubmit}>
             <h2>Отправить пожертвование</h2>
             <label htmlFor="name">Ваше имя</label>
+            {/* add values to the inputs */}
             <input
                 id="name"
                 name="name"
                 type="text"
+                value={formik.values.name}
+                // onChange will change state and value
+                onChange={formik.handleChange}
+                // add onBlur to handle action with input
+                onBlur={formik.handleBlur}
             />
+            {/* add touched to see if there were some actions with input */}
+            {formik.errors.name && formik.touched.name ? <div> {formik.errors.name}</div> : null}
             <label htmlFor="email">Ваша почта</label>
             <input
                 id="email"
                 name="email"
                 type="email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+
             />
+            {formik.errors.email && formik.touched.email ? <div> {formik.errors.email}</div> : null}
             <label htmlFor="amount">Количество</label>
             <input
                 id="amount"
                 name="amount"
                 type="number"
+                value={formik.values.amount}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+
+
             />
             <label htmlFor="currency">Валюта</label>
             <select
                 id="currency"
-                name="currency">
+                name="currency"
+                value={formik.values.currency}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+
+                >
                     <option value="">Выберите валюту</option>
                     <option value="USD">USD</option>
                     <option value="UAH">UAH</option>
@@ -35,9 +100,17 @@ const Form = () => {
             <textarea 
                 id="text"
                 name="text"
+                value={formik.values.text}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+
             />
             <label className="checkbox">
-                <input name="terms" type="checkbox" />
+                <input name="terms" type="checkbox"                 
+                value={formik.values.terms}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                />
                 Соглашаетесь с политикой конфиденциальности?
             </label>
             <button type="submit">Отправить</button>
